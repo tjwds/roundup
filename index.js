2b03adce042eddc0106d4a1d516fbefdaeb55a19
Joe Woods <code@joewoods.dev>
2021-03-14 09:50:52 -0400

    Make the tag configurable

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const { tokens } = require('./tokens');
 const weekAgo = new Date();
 weekAgo.setDate(weekAgo.getDate() - 7)
 
-fetch(`https://api.pinboard.in/v1/posts/recent?auth_token=${tokens.user}:${tokens.password}&format=json&tag=roundup`)
+fetch(`https://api.pinboard.in/v1/posts/recent?auth_token=${tokens.user}:${tokens.password}&format=json&tag=${tokens.tagName}`)
     .then(res => res.json())
     .then(res => {
         res.posts.forEach(post => {
             if (new Date(post.time) > weekAgo) {
-                const tags = post.tags.split(' ').filter(x => x !== 'roundup').join(', ')
+                const tags = post.tags.split(' ').filter(x => x !== tokens.tagName).join(', ')
                 console.log(`[${post.description}](${post.href})  `)
                 if (post.extended) {
                     console.log(post.extended);
--- tokens.example.js
+++ tokens.example.js
@@ -1,6 +1,7 @@
 const tokens = {
     user: 'username',
     password: '1234',
+    tagName: 'roundup',
 }
 
 module.exports = { tokens };
