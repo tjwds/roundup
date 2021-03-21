97974acaff244a3d5fbf1320e600f7b5dbdd1646
Joe Woods <code@joewoods.dev>
2021-03-21 08:24:24 -0400

    Add header to output

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const { tokens } = require('./tokens');
 const weekAgo = new Date();
 weekAgo.setDate(weekAgo.getDate() - 8)
 
+const now = new Date();
+const year = now.getFullYear();
+const month = String(now.getMonth() + 1).padStart(2, '0');
+const date = String(now.getDate()).padStart(2, '0');
+console.log(`+++
+title="article roundup: ${year}${month}${date}"
+date=${year}-${month}-${date}
++++
+`);
 fetch(`https://api.pinboard.in/v1/posts/recent?auth_token=${tokens.apiPhrase}&format=json&tag=${tokens.tagName}`)
     .then(res => res.json())
     .then(res => {
